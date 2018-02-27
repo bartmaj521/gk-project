0b6c9bbe73d5bf9e52edd66243de8b494f37aabd
bartmaj521 <bartmaj@windowslive.com>
2018-02-27 15:17:58 +0100

    Test scene created.

---
+++ The Final Hour/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public Transform Player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LateUpdate()
+    {
+        Vector3 position = Player.position;
+        position.y = transform.position.y;
+        position.x -= 3;
+        transform.position = position;
+        transform.LookAt(Player);
+    }
+}
---
+++ The Final Hour/Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float Speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.W))
+        {
+            Vector3 lol = transform.position;
+            lol.x += Speed * Time.deltaTime;
+            transform.position = lol;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 lol = transform.position;
+            lol.x -= Speed * Time.deltaTime;
+            transform.position = lol;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 lol = transform.position;
+            lol.z += Speed * Time.deltaTime;
+            transform.position = lol;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 lol = transform.position;
+            lol.z -= Speed * Time.deltaTime;
+            transform.position = lol;
+        }
+    }
+}
